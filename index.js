8a02c75b8fa25a3cca16f2f60da2254c97682cfa
shemiramoth <mira.mugo@gmail.com>
2023-03-10 10:46:53 +0300

    lesson1

---
+++ index.js
@@ -0,0 +1,20 @@
+let num = 30
+if (num<10){
+    console.log("Number is less than 10 ")
+}else if(num>20){
+    console.log("Number is greater than 20")
+}
+
+let country = "Chad"
+let age =  17
+
+if (age >= 18){
+    console.log("Accepted")
+}else{
+    console.log("Not Accepted")
+}if(country=="Togo"){
+    console.log("Is true")
+}else{
+    console.log("Is false")
+}
+
